21944f08611364829960b531eab81501f3aed220
cmarti <cmarti@quechoisir.org>
2022-12-19 13:45:56 +0100

    brouillon day17part2

--- day17/puzzle2.php
+++ day17/puzzle2.php
@@ -2,34 +2,35 @@
 
 $rocks = [
     0 => [
-        ['#', '#', '#', '#']
+        ['1', '1', '1', '1']
     ],
     1 => [
-        ['.', '#', '.'],
-        ['#', '#', '#'],
-        ['.', '#', '.'],
+        ['.', '2', '.'],
+        ['2', '2', '2'],
+        ['.', '2', '.'],
     ],
     2 => [
-        ['.', '.', '#'],
-        ['.', '.', '#'],
-        ['#', '#', '#'],
+        ['.', '.', '3'],
+        ['.', '.', '3'],
+        ['3', '3', '3'],
     ],
     3 => [
-        ['#'],
-        ['#'],
-        ['#'],
-        ['#'],
+        ['4'],
+        ['4'],
+        ['4'],
+        ['4'],
     ],
     4 => [
-        ['#', '#'],
-        ['#', '#'],
+        ['5', '5'],
+        ['5', '5'],
     ],
 ];
 
-$gas = str_split(file_get_contents(__DIR__."/input"));
+$solution = 0;
+$gas = str_split(trim(file_get_contents(__DIR__."/input")));
 $map = [];
-$limit = [-1,-1,-1,-1,-1,-1,-1];
-for($i = 0 ; $i< 1000000000000 ; $i++){
+$max_y = 0;
+for($i = 0 ; $i< 2022 ; $i++){
     $current_rock = current($rocks) ? current($rocks):reset($rocks);
     next($rocks);
     $fall = true;
@@ -39,7 +40,7 @@ for($i = 0 ; $i< 1000000000000 ; $i++){
         next($gas);
         $new_current_rock_position = $current_rock_position;
         $new_current_rock_position['x'] = ($g == '>') ? $new_current_rock_position['x']+1:$new_current_rock_position['x']-1;
-
+  
         if(test_position($current_rock, $new_current_rock_position, $map))
             $current_rock_position = $new_current_rock_position;
 
@@ -54,17 +55,15 @@ for($i = 0 ; $i< 1000000000000 ; $i++){
             update_map($current_rock, $current_rock_position, $map);
             $fall = false;
             // draw();
-            // print_r($limit);
+            // print_r($map);
             // readline();
         }
     }
-    echo $i."\r";
 }
-print_r($limit);
-// echo "\nSolution : ".count($map);
+// draw();
+echo "\nSolution : ".$max_y;
 
 function test_position(array $current_rock, array $new_current_rock_position, array $map) :bool {
-    global $limit;
     // var_dump($new_current_rock_position);
     if($new_current_rock_position['x'] == -1 || $new_current_rock_position['x']+count($current_rock[0]) == 8 || $new_current_rock_position['y'] == -1)
         return false;
@@ -72,7 +71,7 @@ function test_position(array $current_rock, array $new_current_rock_position, ar
     $current_rock = array_reverse($current_rock);
     foreach (($current_rock) as $y => $line) {
         foreach ($line as $x => $char) {
-            if($char == '#' && ($limit[$new_current_rock_position['x'] + $x] == $new_current_rock_position['y'] + $y))
+            if($char != '.' && (isset($map[$new_current_rock_position['y'] + $y][$new_current_rock_position['x'] + $x]) && $map[$new_current_rock_position['y'] + $y][$new_current_rock_position['x'] + $x] != '.'))
                 return false;
         }
     }
@@ -80,15 +79,24 @@ function test_position(array $current_rock, array $new_current_rock_position, ar
 }
 
 function update_map(array $current_rock, array $current_rock_position, array &$map) :void {
-    global $limit;
+    global $solution;
+    global $max_y;
     $current_rock = array_reverse($current_rock);
+    $i = 0;
     foreach (($current_rock) as $y => $line) {
         foreach ($line as $x => $char) {
-            // $map[$current_rock_position['y'] + $y][$current_rock_position['x'] + $x] = $current_rock[$y][$x];
-            if($char == '#')
-                $limit[$current_rock_position['x'] + $x] = $current_rock_position['y'] + $y;
-                // print_r($limit);
+            if($char != '.') {
+                $map[$current_rock_position['y'] + $y][$current_rock_position['x'] + $x] = $current_rock[$y][$x];
+                if($current_rock_position['y'] + $y > $max_y)
+                    $max_y = $current_rock_position['y'] + $y;
+            }
         }
+    }
+
+    if(count($map) >= 10){
+        print_r($map);
+        $solution += count($map) - 10;
+        $map = array_slice($map, -10, null, true);
     }
 }
 
@@ -97,12 +105,15 @@ function draw() :void{
     $m = array_reverse($map);
 
     foreach ($m as $y => $line) {
-        echo "|";
+        echo str_pad($y, 3)."|";
         for($i = 0;$i <7;$i++){
             echo $m[$y][$i] ?? '.';
         }
         echo "|";
         echo "\n";
+        // if($y > 99)
+            // break;
+
     }
     echo "\n";
     echo "\n";
