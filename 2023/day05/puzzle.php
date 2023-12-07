4edd0ec1a7023741e073a82f43df6f53295c95d8
Christophe Marti <cmarti@quechoisir.org>
2023-12-07 20:17:53 +0100

    day05 part 1

--- 2023/day05/puzzle.php
+++ 2023/day05/puzzle.php
@@ -1,36 +1,46 @@
 <?php
 $input = fopen(__DIR__."/input", "r");
 
-$result = 0;
-$converter = [];
+$location = false;
 
 $line = trim(fgets($input), "\n");
 $seeds = explode(' ', substr($line, 7));
-print_r($seeds);
 
+foreach ($seeds as $seed) {
+    $source = $seed;
 
-while(!feof($input)) {
+    $input = fopen(__DIR__."/input", "r");
     $line = trim(fgets($input), "\n");
-    if($line == '')
-        continue;
+    while(!feof($input)) {
+        $line = trim(fgets($input), "\n");
+        if($line == '')
+            continue;
 
-    if(preg_match("#(\w+)-to-(\w+) map#", $line, $m)){
-        print_r($m);
-        while(true) {
-            $line = trim(fgets($input), "\n");
-            if($line == "" || feof($input)){
-                break;
+        if(preg_match("#(\w+)-to-(\w+) map#", $line, $m)){
+            $destination = false;
+            while(true) {
+                $line = trim(fgets($input), "\n");
+                if($line == "" || feof($input)){
+                    break;
+                }
+                $numbers = explode(' ', $line);
+
+                if($source >= $numbers[1] && $source < $numbers[1] + $numbers[2]){
+                    $destination = $source + ($numbers[0] - $numbers[1]);
+                    break;
+                }
             }
-            $numbers = explode(' ', $line);
-            for($i = 0; $i < $numbers[2]; $i++){
-                $converter[$m['0']][$m['1']][$numbers[0] + $i] = $numbers[1] + $i;
+            if(!$destination){
+                $destination = $source;
+            }
+            $source = $destination;
+            if($m[2] == 'location'){
+                if($location === false || $location > $destination){
+                    $location = $destination;
+                }
             }
         }
     }
-
-
 }
-print_r($converter);
 
-// echo "Solution : '$result'";
-
+echo "Solution : '$location'";
--- 2023/day05/puzzle2.php
+++ 2023/day05/puzzle2.php
@@ -1,39 +1,64 @@
 <?php
-$lines = file( __DIR__."/input" );
+$input = fopen(__DIR__."/input", "r");
 
-$new_lines = [];
 $result = 0;
-foreach ( $lines as $line ) {
-    $line = trim($line);
-    preg_match('#Card ([ 0-9]+): ([ 0-9]+) \| ([ 0-9]+)#', $line, $m);
-    $winning_numbers = explode(' ', $m[2]);
-    $numbers = explode(' ', $m[3]);
-    $intersect = array_intersect($winning_numbers, $numbers);
+$location = false;
+$converter = [];
 
-    foreach($intersect as $key => $n) {
-        if($n == '') {
-            unset($intersect[$key]);
-            continue;
-        }
-    }
+$line = trim(fgets($input), "\n");
+$seeds = explode(' ', substr($line, 7));
 
-    $new_lines[trim($m[1])] = [
-        'copy' => 1,
-        'intersect' => $intersect,
+$ranges = [];
+for($i = 0; $i< count($seeds);$i = $i+2){
+    $ranges[] = [
+        "start" => $seeds[$i],
+        "range" => $seeds[$i+1],
     ];
 }
+print_r($ranges);
+die();
 
-foreach ( $new_lines as $card => $line ) {
-    $points = 0;
-    if(count($line['intersect']) > 0){ 
-        for($intersect = 1; $intersect <= count($line['intersect']); $intersect++){
-            $new_lines[$card + $intersect]['copy'] += $new_lines[$card]['copy'];
+
+
+
+$input = fopen(__DIR__."/input", "r");
+$line = trim(fgets($input), "\n");
+while(!feof($input)) {
+    $line = trim(fgets($input), "\n");
+    if($line == '')
+        continue;
+
+    
+    if(preg_match("#(\w+)-to-(\w+) map#", $line, $m)){
+        $tmp_ranges = [];
+        while(true) {
+            $line = trim(fgets($input), "\n");
+            $numbers = explode(' ', $line);
+            $tmp_ranges[] = [
+                'start' => $numbers[1],
+                'range' => $numbers[2],
+                'destination_start' => $numbers[0],
+            ];
         }
-        foreach($line['intersect'] as $n) {
-            $points = ($points == 0 ? 1:$points*2);
+
+        $new_ranges = [];
+        foreach ($ranges as $key_range => $range) {
+            foreach ($tmp_ranges as $tmp_range) {
+                if($range['start'] >= $tmp_range['start'] && $range['start'] < $tmp_range['start'] + $tmp_range['range']){
+                    $new_range['start'] = $range['start'] + ($tmp_range['destination_start'] - $tmp_range['start']);
+
+                    if($range['start']+$range['range'] < $tmp_range['start'] + $tmp_range['range']){
+                        $new_range['range'] = $range['range'];
+                    }
+                    else{
+                        $new_range['range'] = ($tmp_range['start'] + $tmp_range['range']) - $range['start'];
+                    }
+                }
+            }
         }
+   
+
+
     }
-    $result += $new_lines[$card]['copy'];
 }
 
-echo "Solution : '$result'";
